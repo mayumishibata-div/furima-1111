561869b2a5c1f57352b7916be6cb517f7e497592
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-02-25 18:31:18 +0900

    shipping_areaにバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,12 @@ class Item < ApplicationRecord
   belongs_to :item_category
   belongs_to :item_condition
   belongs_to :shipping_cost
+  belongs_to :shipping_area
 
 
   #ジャンルの選択が「---」の時は保存できないようにする
   validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
