82f1b369d5d57ea9a7d5b326105573ebd3dfa6b5
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-02-25 18:26:18 +0900

    item_conditionにバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
+  belongs_to :item_condition
+
 
   #ジャンルの選択が「---」の時は保存できないようにする
   validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
