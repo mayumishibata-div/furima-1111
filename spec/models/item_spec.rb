862ebc510d1f70020d676a8c3f6207fdb629b661
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-02-26 17:11:15 +0900

    テストコードの実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    
+    context '商品が出品できる場合' do
+      
+      it '全てのバリデーションを通過する時' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品が出品できない場合' do
+
+      it '画像が空では出品できないこと' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '商品名がないと出品できないこと' do
+        @item.item_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+
+      it '商品説明がないと出品できないこと' do
+        @item.item_description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item description can't be blank")
+      end
+      
+      it 'カテゴリが未選択(---)の場合は出品できないこと' do
+        @item.item_category_id = 1
+        # item = FactoryBot.build(:item, item_category_id: 1)
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item category can't be blank")
+      end
+
+      it '商品の状態が未選択(---)の場合は出品できないこと' do
+        @item.item_condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition can't be blank")
+      end
+
+      it '送料が未選択(---)の場合は出品できないこと' do
+        @item.shipping_cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
+      end
+
+      it '配送料地域が未選択(---)の場合は出品できないこと' do
+        @item.shipping_area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area can't be blank")
+      end
+
+      it '配送時間が未選択(---)の場合は出品できないこと' do
+        @item.delivery_time_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time can't be blank")
+      end
+
+      it '価格が入力されていないと出品できないこと' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      
+    end
+  end
 end
