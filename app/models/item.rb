a9cd4211ea187448cdae800c727031a2b584e135
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-02-26 17:10:29 +0900

    priceのバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_description, presence: true
+  validates :price, presence: true
 
   #ジャンルの選択が「---」の時は保存できないようにする
   validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
