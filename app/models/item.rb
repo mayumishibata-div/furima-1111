7cb13da77d87374c99f7106011606d11d65293d5
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-02-27 10:48:53 +0900

    入力文字数・価格においてのバリデーションを追加設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,10 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :image, presence: true
-  validates :item_name, presence: true
-  validates :item_description, presence: true
-  validates :price, presence: true
+  validates :item_name, presence: true, length: { maximum: 40 }
+  validates :item_description, presence: true,length: { maximum: 1000 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, format: { with: /\A[0-9]+\z/, message: "は半角数字のみで入力してください。" }
 
   #ジャンルの選択が「---」の時は保存できないようにする
   validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
