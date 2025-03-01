724141d8c99e266ebcad51ae0a13b057eb74ada4
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-01 15:38:15 +0900

    DRY（Don't Repeat Yourself）の原則にそってコードをまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -37,7 +37,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
     redirect_to root_path
 
