e130d17877a17353c7635a79732d9c9fbf1de141
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-13 16:13:32 +0900

    renderが実行された場合でもJavaScriptのコードが実行されるように実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_***********************')
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -29,3 +30,4 @@ const pay = () => {
 };
 
 window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
