4bc972bc5ba8d834e44b41bdfbf1bd47e82bce45
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-13 11:49:33 +0900

    テストコードの準備

---
+++ spec/factories/order_shipping_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_shipping_address do
+    post_code { '123-4567' }
+    shipping_area_id { 1 }
+    city { '東京都' }
+    address { '1-1' }
+    building_name { '東京ハイツ' }
+    tel_number {  '09012345678' }
+  end
+end
