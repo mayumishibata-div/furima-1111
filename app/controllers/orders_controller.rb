0cd3326e5b2749e7f258f2840e766d064551ca48
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-14 14:15:09 +0900

    自分が出品した商品の購入ページに遷移できないように実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,10 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create ]
 
   def index
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order = Order.new
     @order_shipping_address = OrderShippingAddress.new
