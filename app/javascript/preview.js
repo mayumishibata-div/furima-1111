e3a96398675e819cbe1c2cc45a56213957eb7fc5
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-18 14:08:37 +0900

    JavaScriptで表示するスペースのHTML要素を取得

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,7 @@
 document.addEventListener('turbo:load', function(){
   // 新規投稿・編集ページのフォームを取得
   const postForm = document.getElementById('new_post');
+  const previewList = document.getElementById('previews');
 
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
