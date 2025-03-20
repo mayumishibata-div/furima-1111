765789dfe24be584be3401d1b73a81f9a0fcf38d
mayumishibata-div <mayumi.shibata@di-v.co.jp>
2025-03-20 10:51:24 +0900

    削除ボタンに削除の機能を実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -28,6 +28,10 @@ document.addEventListener('turbo:load', function(){
     deleteButton.setAttribute("class", "image-delete-button");
     deleteButton.innerText = "削除";
 
+    // 削除ボタンをクリックしたらプレビューとfile_fieldを削除させる
+    deleteButton.addEventListener("click", () => deleteImage(dataIndex));
+
+
     // 生成したHTMLの要素をブラウザに表示させる
     previewWrapper.appendChild(previewImage);
     previewWrapper.appendChild(deleteButton);
